994681ca4c31136c94038a2e014c6dbe291fd70a
RajPrft <raj.solapure@perficient.com>
2026-05-10 17:17:20 +0530

    feat(events): updates

--- pages/0_all_steps.py
+++ pages/0_all_steps.py
@@ -2,7 +2,7 @@ import streamlit as st
 import importlib
 st.title('All Steps Overview')
 st.markdown('---')
-modules = ['pages.1_backend_technologies', 'pages.2_frontend_technologies', 'pages.3_database_technologies', 'pages.4_cloud_technologies', 'pages.5_hybrid_technologies', 'pages.6_category_technologies']
+modules = ['ResumeShortListing.pages.1_backend_technologies', 'ResumeShortListing.pages.2_frontend_technologies', 'ResumeShortListing.pages.3_database_technologies', 'ResumeShortListing.pages.4_cloud_technologies', 'ResumeShortListing.pages.5_hybrid_technologies', 'ResumeShortListing.pages.6_category_technologies']
 for mod in modules:
     st.header(mod.split('.')[-1].replace('_',' ').title())
     with st.expander(mod, expanded=True):
